3fb030524b7166822c46c06b1344b37c6a3363dc
Ricketysole <73779797+Ricketysole@users.noreply.github.com>
2020-11-01 05:40:50 -0600

    Update text_generation.py

--- text_generation.py
+++ text_generation.py
@@ -1,16 +1,15 @@
 import requests, json
 
 def generate_text(text):
-	var = text
 	endpoint = "https://api.inferkit.com/v1/models/standard/generate"
+	#refer to https://inferkit.com/docs/generation-api to customize your data request
 	data = json.dumps({
 	  "prompt": {
-	    "text": "{}".format(var)
+	    "text": "{}".format(text)
 	  },
 	  "length": 500
 	})
-	headers = {"Authorization": "Bearer 0f058a4b-e73c-4d02-ac24-7838af159b84"}
+	headers = {"Authorization": "Bearer {your token here}"}
 	response = requests.post(endpoint, data=data, headers=headers).json()
 
-	print(response)
 	return (var + ' ' + response["data"]["text"])
